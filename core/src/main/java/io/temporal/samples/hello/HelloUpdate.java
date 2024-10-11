40296964146f7dc2b488722f165f309db4bda427
Steve Androulakis <steve.androulakis@gmail.com>
2024-10-11 09:20:01 -0700

    updatewithstart

--- core/src/main/java/io/temporal/samples/hello/HelloUpdate.java
+++ core/src/main/java/io/temporal/samples/hello/HelloUpdate.java
@@ -7,15 +7,96 @@ import io.temporal.serviceclient.WorkflowServiceStubs;
 import io.temporal.worker.Worker;
 import io.temporal.worker.WorkerFactory;
 import io.temporal.workflow.*;
+
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class HelloUpdate {
-  static final String TASK_QUEUE = "HelloUpdateTaskQueue";
-  static final String randomId = java.util.UUID.randomUUID().toString();
-  static final String WORKFLOW_ID = "GreetingWorkflow-" + randomId;
+  private static final String TASK_QUEUE = "HelloUpdateTaskQueue";
+  private static final String WORKFLOW_ID = "GreetingWorkflow-" + UUID.randomUUID();
+
+  public static void main(String[] args) throws Exception {
+    WorkflowClient client = setupWorkflowClient();
+    startWorker(client);
+    runWorkflowWithUpdates(client);
+  }
+
+  private static WorkflowClient setupWorkflowClient() {
+    WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
+    return WorkflowClient.newInstance(service);
+  }
+
+  private static void startWorker(WorkflowClient client) {
+    WorkerFactory factory = WorkerFactory.newInstance(client);
+    Worker worker = factory.newWorker(TASK_QUEUE);
+
+    worker.registerWorkflowImplementationTypes(GreetingWorkflowImpl.class);
+    worker.registerActivitiesImplementations(new HelloActivity.GreetingActivitiesImpl());
+
+    factory.start();
+    System.out.println("Worker started");
+  }
+
+  private static void runWorkflowWithUpdates(WorkflowClient client) throws InterruptedException {
+    WorkflowOptions options = WorkflowOptions.newBuilder()
+            .setTaskQueue(TASK_QUEUE)
+            .setWorkflowId(WORKFLOW_ID)
+            .build();
+
+    WorkflowStub workflowStub = client.newUntypedWorkflowStub("GreetingWorkflow", options);
+
+    // Run the workflow with UpdateWithStart
+    performUpdateWithStart(workflowStub);
+
+    // Sleep for a bit before sending the next update
+    Thread.sleep(1000);
+
+    // Send another update
+    performUpdate(workflowStub, "Steve test 2");
+
+    // Sleep for a bit before sending the next update
+    Thread.sleep(1000);
+
+    // Send another update
+    performUpdate(workflowStub, "Steve test 3");
+
+    // Sleep for a bit before sending the next update
+    Thread.sleep(1000);
+
+    // Send another update
+    performUpdate(workflowStub, "Steve test 4");
+
+    // Wait for the workflow to complete
+    waitForWorkflowCompletion(workflowStub);
+  }
+
+  private static void performUpdateWithStart(WorkflowStub workflowStub) {
+    System.out.println("Starting workflow with UpdateWithStart");
+    UpdateWithStartWorkflowOperation<String> update = UpdateWithStartWorkflowOperation.newBuilder(
+                    "addGreeting", String.class, new Object[]{"Steve test 1"})
+            .setWaitForStage(WorkflowUpdateStage.COMPLETED)
+            .build();
+
+    WorkflowUpdateHandle<String> updateHandle = workflowStub.updateWithStart(update);
+    CompletableFuture<String> updateFuture = updateHandle.getResultAsync();
+    String result = updateFuture.join();
+    System.out.println("UpdateWithStart result: " + result);
+  }
+
+  private static void performUpdate(WorkflowStub workflowStub, String greeting) {
+    System.out.println("Performing update on existing workflow");
+    String updateResult = workflowStub.update("addGreeting", String.class, greeting);
+    System.out.println("Update result. Queue size is: " + updateResult);
+  }
+
+  private static void waitForWorkflowCompletion(WorkflowStub workflowStub) {
+    System.out.println("Waiting for workflow to complete");
+    List<String> result = workflowStub.getResult(List.class);
+    System.out.println("Workflow completed with result: " + result);
+  }
 
   @WorkflowInterface
   public interface GreetingWorkflow {
@@ -32,23 +113,23 @@ public class HelloUpdate {
   public static class GreetingWorkflowImpl implements GreetingWorkflow {
     private final List<String> messageQueue = new ArrayList<>(10);
     private final List<String> receivedMessages = new ArrayList<>(10);
-    private final HelloActivity.GreetingActivities activities =
-        Workflow.newActivityStub(
+    private final HelloActivity.GreetingActivities activities = Workflow.newActivityStub(
             HelloActivity.GreetingActivities.class,
             ActivityOptions.newBuilder().setStartToCloseTimeout(Duration.ofSeconds(2)).build());
 
     @Override
     public List<String> getGreetings() {
-      Workflow.sleep(Duration.ofSeconds(10));
+      Workflow.sleep(Duration.ofSeconds(12));
       return messageQueue;
     }
 
     @Override
     public int addGreeting(String name) {
       if (name.isEmpty()) {
-        throw ApplicationFailure.newFailure("Cannot greet someone with an empty name", "Failure");
+        throw ApplicationFailure.newFailure("Cannot greet someone with an empty name", "EmptyNameFailure");
       }
-      messageQueue.add(activities.composeGreeting("Hello", name));
+      String greeting = activities.composeGreeting("Hello", name);
+      messageQueue.add(greeting);
       return receivedMessages.size() + messageQueue.size();
     }
 
@@ -58,50 +139,5 @@ public class HelloUpdate {
         throw new IllegalStateException("Only 10 greetings may be added");
       }
     }
-  }
-
-  public static void main(String[] args) throws Exception {
-    WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
-    WorkflowClient client = WorkflowClient.newInstance(service);
-    WorkerFactory factory = WorkerFactory.newInstance(client);
-    Worker worker = factory.newWorker(TASK_QUEUE);
-
-    worker.registerWorkflowImplementationTypes(GreetingWorkflowImpl.class);
-    worker.registerActivitiesImplementations(new HelloActivity.GreetingActivitiesImpl());
-    factory.start();
-
-    WorkflowOptions workflowOptions =
-        WorkflowOptions.newBuilder().setTaskQueue(TASK_QUEUE).setWorkflowId(WORKFLOW_ID).build();
-
-    WorkflowStub workflowStub1 = client.newUntypedWorkflowStub("GreetingWorkflow", workflowOptions);
-
-    // UpdateWithStart workflow
-    UpdateWithStartWorkflowOperation<String> update1 =
-        UpdateWithStartWorkflowOperation.newBuilder(
-                "addGreeting", String.class, new Object[] {0, "Steve test 1"})
-            .setWaitForStage(WorkflowUpdateStage.COMPLETED)
-            .build();
-
-    // Get the handle for the updateWithStart workflow
-    WorkflowUpdateHandle<String> updateHandle1 = workflowStub1.updateWithStart(update1);
-    System.out.println("UpdateWithStart workflow ID: " + WORKFLOW_ID + " started");
-
-    // Get the result of the updateWithStart workflow's initial update
-    CompletableFuture<String> updateWithStartFuture = updateHandle1.getResultAsync();
-    String updateWithStartFutureResult = updateWithStartFuture.join();
-    System.out.println("Result of updateWithStart: " + updateWithStartFutureResult);
-
-    // Sleep for 5 seconds
-    Thread.sleep(5000);
-
-    // Update the workflow
-    System.out.println("\nSending update to workflow");
-    String updateResult = workflowStub1.update("addGreeting", String.class, 1, "Steve test 2");
-    System.out.println("Result of update: " + updateResult);
-
-    System.out.println("\nWaiting for workflow to complete");
-    List<String> result = workflowStub1.getResult(List.class);
-    System.out.println("Workflow completed with result: " + result);
-    System.exit(0);
   }
 }
